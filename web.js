a45dc7d598a5783e9ba040e14d0bae6f2d3d9c78
Mark Everidge <mark.ev@gmail.com>
2013-07-07 17:40:26 +0000

    Changed web.js to read from index.html -- fixed

--- web.js
+++ web.js
@@ -3,6 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+
+	var fs = require('fs');
 	//read from file
 	var myBuff = fs.readFileSync(index.html);
 	
